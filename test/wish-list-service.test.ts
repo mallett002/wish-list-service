6222b3bc39e6a145cc2e88c287c8be4221a0c9fe
Will Mallett <mallett002@gmail.com>
2023-05-13 18:05:14 -0500

    Initial commit

---
+++ test/wish-list-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WishListService from '../lib/wish-list-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wish-list-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WishListService.WishListServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
