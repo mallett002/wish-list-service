43a66166c5a5ce1218769f5457c9076978f2570b
William Mallett <mallett002@gmail.com>
2023-07-05 07:02:35 -0500

    added member-gift script to insert random data

---
+++ functions/get-family-board/test/member-gift.js
@@ -0,0 +1,172 @@
+const { DynamoDBClient, BatchWriteItemCommand, QueryCommand } = require('@aws-sdk/client-dynamodb');
+const Chance = require('chance');
+
+const chance = new Chance();
+const client = new DynamoDBClient({ region: "us-east-1" });
+
+function createMemberInput() {
+    return {
+        PutRequest: {
+            Item: {
+                PK: {
+                    S: `MEMBER#${chance.guid()}`
+                },
+                SK: {
+                    S: `PROFILE`
+                },
+                email: {
+                    S: chance.email()
+                },
+                alias: {
+                    S: chance.first()
+                }
+            }
+        }
+    };
+}
+
+function createFamilyInput(familyId) {
+    return {
+        "PutRequest": {
+            "Item": {
+                "PK": {
+                    "S": `FAMILY#${familyId}`
+                },
+                "SK": {
+                    "S": 'MEMBER#BOARD'
+                },
+                "familyName": {
+                    "S": chance.last()
+                },
+                "familyImage": {
+                    "S": chance.string()
+                },
+            },
+        }
+    };
+}
+
+function createFamilyMember(familyId, alias, email, memberId) {
+    return {
+        "PutRequest": {
+            "Item": {
+                "PK": {
+                    "S": `FAMILY#${familyId}`
+                },
+                "SK": {
+                    "S": `MEMBER#${memberId}`
+                },
+                "alias": {
+                    "S": alias
+                },
+                "email": {
+                    "S": email
+                },
+            }
+        }
+    }
+}
+
+function createGift(familyId, memberId) {
+    return {
+        PutRequest: {
+            Item: {
+                PK: {
+                    S: `FAMILY#${familyId}`
+                },
+                SK: {
+                    S: `MEMBER#${memberId}GIFT#${chance.guid()}`
+                },
+                description: {
+                    S: chance.sentence()
+                },
+                link: {
+                    S: chance.url()
+                },
+                title: {
+                    S: chance.string()
+                },
+                purchased: {
+                    BOOL: chance.bool()
+                }
+            }
+        }
+    }
+}
+
+async function putItems(input) {
+    const command = new BatchWriteItemCommand(input);
+
+    const result = await client.send(command);
+
+    console.log(JSON.stringify({result}));
+}
+
+async function queryBoard(familyId) {
+    const command = new QueryCommand({
+        TableName: 'wish-list-table',
+        KeyConditionExpression: `#PK = :PK AND begins_with(#SK, :SK)`,
+        ExpressionAttributeNames: {
+          "#PK": "PK",
+          "#SK": "SK"
+        },
+        ExpressionAttributeValues: {
+          ":PK": {S: `FAMILY#${familyId}`},
+          ":SK": {S: `MEMBER#`}
+        },
+        ConsistentRead: true,
+        ScanIndexForward: true
+      });
+
+      const response = await client.send(command);
+      console.log(JSON.stringify({response}, null, 2));
+}
+
+(async function doIt() {
+    const memberInputs = [];
+    const familyMemberInputs = [];
+    const giftInputs = [];
+
+
+    const familyId = chance.guid();
+
+    const familyInput = createFamilyInput(familyId);
+
+    for (let i = 0; i < 3; i++) {
+        const member = createMemberInput();
+
+        const {PutRequest: {Item: memberItem}} = member;
+        const memberId = memberItem.PK.S.replace('MEMBER#', '');
+
+        const familyMember = createFamilyMember(
+            familyId,
+            memberItem.alias.S,
+            memberItem.email.S,
+            memberId
+        );
+
+        memberInputs.push(member);
+        familyMemberInputs.push(familyMember);
+
+        for (let i = 0; i < chance.d6(); i++) {
+            giftInputs.push(createGift(familyId, memberId));
+        }
+    }
+
+    const itemsToPush = [
+        familyInput,
+        ...familyMemberInputs,
+        ...giftInputs
+    ];
+
+    // console.log({length: itemsToPush.length});
+
+    const input = {
+        "RequestItems": {
+            "wish-list-table": itemsToPush
+        }
+    };
+
+    await putItems(input);
+    await queryBoard(familyId);
+})()
